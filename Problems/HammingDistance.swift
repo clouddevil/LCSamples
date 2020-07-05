f32963c3afe2579bb587615c226ef570b04178f3
Eugene Kalinin <jenia.kalinin@gmail.com>
2020-07-05 18:09:27 +0300

    461. Hamming Distance

---
+++ Problems/HammingDistance.swift
@@ -0,0 +1,20 @@
+
+// 461. Hamming Distance
+
+class Solution {
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        var count = 0
+        var n = x ^ y
+        
+        while n > 0 {
+            n &= n - 1
+            count += 1
+        }
+        
+        return count
+    }
+
+    func hammingDistance0(_ x: Int, _ y: Int) -> Int {
+        return (x ^ y).nonzeroBitCount
+    }
+}
