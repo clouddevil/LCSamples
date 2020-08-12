a739dcd8342a8b60fc94e7b9e27dfc47446e318f
Eugene <jenia.kalinin@gmail.com>
2020-08-12 11:19:38 +0300

    119. Pascal's Triangle II

--- Problems/PascalTriangle.swift
+++ Problems/PascalTriangle.swift
@@ -1,15 +1,13 @@
-
+// 119. Pascal's Triangle II
 
 class PascalTriangle {
-    
     func getRow(_ rowIndex: Int) -> [Int] {
         guard rowIndex >= 0 else {
             return []
         }
-        
         var result = Array(repeating: 0, count: rowIndex + 1)
         result[0] = 1
-        
+
         var i = 1
         while (i <= rowIndex) {
             var j = i
@@ -21,8 +19,8 @@ class PascalTriangle {
         }
         return result
     }
-    
+
 }
 
 
-PascalTriangle().getRow(20)
+//PascalTriangle().getRow(20)
